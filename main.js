78875721754ec24ab482f6dc773aa67f98f0ffa8
LoveClean <641655770@qq.com>
2019-07-24 16:36:30 +0800

    更新

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false;
-Vue.prototype.$token = 'D434AD7C50859B02D93784D9EEE93DA47e8796035fbd4183ac25bc48d4cbbb46';
+Vue.prototype.$token = '';
 Vue.prototype.$tempUrl = 'http://localhost:8088/';
 
 App.mpType = 'app'
